03187c47a0ff0509aa41c716948a8e691211b87e
sonhmai <>
2023-10-24 06:07:10 +0700

    Add Heartbeat

---
+++ modules/spark-simplified/src/main/scala/com/spark/simplified/rpc/message/Heartbeat.scala
@@ -0,0 +1,18 @@
+package com.spark.simplified.rpc.message
+import com.spark.simplified.rpc.{RpcEndpoint, RpcEnv}
+
+/** A heartbeat from executors to the driver. This is a shared message used by several internal
+  * components to convey liveness or execution information for in-progress tasks. It will also
+  * expire the hosts that have not heartbeated for more than spark.network.timeout.
+  * spark.executor.heartbeatInterval should be significantly less than spark.network.timeout.
+  */
+case class Heartbeat(
+    message: String,
+    executorId: String
+)
+
+class HeartbeatReceiver(rpcEnv: RpcEnv) extends RpcEndpoint {
+  override val rpcEnv: RpcEnv                      = this.rpcEnv
+  override def receive: PartialFunction[Any, Unit] = ???
+  override def onStop(): Unit                      = ???
+}
