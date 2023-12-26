180eaa75ea31a12cb173424c9abc44d4a7365c53
sonhmai <>
2023-12-26 13:15:15 +0700

    Add example reading from Kafka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -118,6 +118,7 @@ object Dependencies {
   val sparkDeps: Seq[ModuleID] = Seq(
     "org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-sql"  % sparkVersion,
+    "org.apache.spark" %% "spark-sql-kafka-0-10"  % sparkVersion,
     "org.apache.spark" %% "spark-hive"  % sparkVersion,
   )
 
