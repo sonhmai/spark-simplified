5c74d1d801d8990c26b6cc3b14097596f0095e4c
sonhmai <>
2024-01-02 13:16:50 +0700

    Implement TaskRunner

--- modules/spark-simplified/src/main/scala/com/spark/simplified/deploy/executor/TaskRunner.scala
+++ modules/spark-simplified/src/main/scala/com/spark/simplified/deploy/executor/TaskRunner.scala
@@ -1,9 +1,11 @@
 package com.spark.simplified.deploy.executor
+import com.spark.simplified.scheduler.TaskDescription
 
-class TaskRunner extends Runnable {
+class TaskRunner(
+    executorBackend: ExecutorBackendForScheduler,
+    taskDescription: TaskDescription
+) extends Runnable {
 
- override def run(): Unit = {
-
- }
+  override def run(): Unit = {}
 
 }
