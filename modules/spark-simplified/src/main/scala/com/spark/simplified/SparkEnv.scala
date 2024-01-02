f54dc7008641f4161ec002f72a6bb9e771ee61cf
sonhmai <>
2024-01-02 11:23:50 +0700

    Add SparkEnv

---
+++ modules/spark-simplified/src/main/scala/com/spark/simplified/SparkEnv.scala
@@ -0,0 +1,13 @@
+package com.spark.simplified
+
+/**
+ * Holds all runtime env objects for running Spark process, including
+ *  - serializer
+ *  - RpcEnv
+ *  - block manager
+ *  - map output tracker
+ *  ...
+ */
+class SparkEnv {
+
+}
