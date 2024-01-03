2fbeb5497a7fe28636e20c85a6d381f0c7ae98ef
sonhmai <14060682+sonhmai@users.noreply.github.com>
2024-01-03 10:19:19 +0700

    Implement RpcEndpoint

--- modules/spark-simplified/src/main/scala/com/spark/simplified/rpc/RpcEndpoint.scala
+++ modules/spark-simplified/src/main/scala/com/spark/simplified/rpc/RpcEndpoint.scala
@@ -1,8 +1,33 @@
 package com.spark.simplified.rpc
 
-// an object that can receive RPC messages
+import org.apache.spark.rpc.ThreadSafeRpcEndpoint
+
+/** An end point for the RPC that defines what functions to trigger given a message.
+  *
+  * It is guaranteed that `onStart`, `receive` and `onStop` will be called in sequence.
+  *
+  * The life-cycle of an endpoint is:
+  *
+  * {@code constructor -> onStart -> receive* -> onStop}
+  *
+  * Note: `receive` can be called concurrently. If you want `receive` to be thread-safe, please use
+  * [[ThreadSafeRpcEndpoint]]
+  *
+  * If any error is thrown from one of [[RpcEndpoint]] methods except `onError`, `onError` will be
+  * invoked with the cause. If `onError` throws an error, [[RpcEnv]] will ignore it.
+  */
 trait RpcEndpoint {
   val rpcEnv: RpcEnv
+
+  // PartialFunction[Any, Unit] is a func takes Any as input and output Unit
+
+  /** Process messages from `RpcEndpointRef.send` or `RpcCallContext.reply`. If receiving a
+    * unmatched message, `SparkException` will be thrown and sent to `onError`.
+    */
   def receive: PartialFunction[Any, Unit]
-  def onStop(): Unit
+
+  /** Process messages from `RpcEndpointRef.ask`. If receiving a unmatched message,
+    * `SparkException` will be thrown and sent to `onError`.
+    */
+  def receiveAndReply(): PartialFunction[Any, Unit]
 }
