6858001972e825ca3405086679d4cdd5fce900b1
sonhmai <14060682+sonhmai@users.noreply.github.com>
2024-01-12 06:05:46 +0700

    Add TaskContext

---
+++ modules/spark-simplified/src/main/scala/com/spark/simplified/work/task/ShuffleMapTask.scala
@@ -0,0 +1,11 @@
+package com.spark.simplified.work.task
+
+import com.spark.simplified.util.Logging
+
+/**
+  * A ShuffleMapTask divides the elements of an RDD into multiple buckets (based on a partitioner
+  * specified in the ShuffleDependency).
+  */
+class ShuffleMapTask extends Task with Logging {
+
+}
